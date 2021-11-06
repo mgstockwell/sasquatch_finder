3c5c3ffe0ca06e65a43c73309314269f1feed550
Mark <mark.g.stockwell@gmail.com>
2021-11-05 22:43:44 -0400

    Add EXPORT var command

--- gcloud-setup.sh
+++ gcloud-setup.sh
@@ -1,11 +1,11 @@
-export GOOGLE_CLOUD_PROJECT=msd8654-434-bfro-dev
+export $GOOGLE_CLOUD_PROJECT=$GOOGLE_CLOUD_PROJECT
 export GOOGLE_BILLING_ACCT=01278E-34A2F6-83795E
 gcloud info
-gcloud projects create msd8654-434-bfro-dev --labels=env=dev
-gcloud config set project msd8654-434-bfro-dev
+gcloud projects create $GOOGLE_CLOUD_PROJECT --labels=env=dev
+gcloud config set project $GOOGLE_CLOUD_PROJECT
 echo Find the Billing Acccounts....
 gcloud alpha billing accounts list
-gcloud beta billing projects link msd8654-434-bfro-dev --billing-account=01278E-34A2F6-83795E
+gcloud beta billing projects link $GOOGLE_CLOUD_PROJECT --billing-account=$GOOGLE_BILLING_ACCT
 
 gcloud services enable compute.googleapis.com bigquery.googleapis.com bigquerystorage.googleapis.com \
 bigquerydatatransfer.googleapis.com  cloudapis.googleapis.com cloudfunctions.googleapis.com \
@@ -13,10 +13,10 @@ elevation-backend.googleapis.com pubsub.googleapis.com run.googleapis.com storag
 storage-component.googleapis.com storage.googleapis.com
 
 gcloud iam service-accounts create terraform-service-acct --display-name="Terraform Service Account"
-gcloud projects add-iam-policy-binding msd8654-434-bfro-dev --member='serviceAccount:terraform-service-acct@msd86
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member='serviceAccount:terraform-service-acct@msd86
 54-434-bfro-dev.iam.gserviceaccount.com' --role='roles/editor'
 
 gcloud iam service-accounts keys create terraform-service-acct-key.json \
- --iam-account=terraform-service-acct@msd8654-434-bfro-dev.iam.gserviceaccount.com
+ --iam-account=terraform-service-acct@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com
 
 
