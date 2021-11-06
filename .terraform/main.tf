3186b16bda1c247cbfc3d8021b462a3553ec4f12
Mark <mark.g.stockwell@gmail.com>
2021-11-05 23:08:11 -0400

    tf

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -12,7 +12,7 @@ provider "google" {
   project     = "msd8654-434-bfro-dev"
 }
 
-resource "google_storage_bucket"  {
+resource "google_storage_bucket" "bucket" {
   name          = "bfro_data"
   location      = "US"
   force_destroy = true
