0cceeb6e4f11719897ee57403cb27346e2316dcf
mgstockwell <49590350+mgstockwell@users.noreply.github.com>
2021-11-27 00:43:12 -0500

    Update bq_ddl.sql

--- data/bq_ddl.sql
+++ data/bq_ddl.sql
@@ -40,3 +40,77 @@ OPTIONS (
   uris = ['gs://bfro_data/bfro_reports_geocoded.csv']
 )
 ;
+
+CREATE OR REPLACE TABLE
+  `bfro.bfro_reports_geocoded_final` AS
+SELECT
+  base.*,
+  ST_GEOGPOINT(base.longitude,
+    base.latitude) geogpoint,
+  zip.zip_code,
+  zip.area_land_meters,
+  uscb.population,
+  ROUND(uscb.population/(zip.area_land_meters/1000000)) pop_density_sqkm,
+  counties.county_fips_code,
+  zip.county AS zip_county,
+  zip.state_fips_code,
+  zip.state_code,
+  zip.state_name,
+  hz.zone AS hardiness_zone_code,
+  LEFT(zone,LENGTH(zone)-1) hardiness_zone_num,
+  elevations.elevation,
+  pcp.yearly_precipitation
+FROM
+  `bfro.bfro_reports_geocoded_csv` AS base
+INNER JOIN (
+  SELECT
+    geo.number,
+    z.zip_code,
+    CAST(z.zip_code AS int64) AS zip_code_int64,
+    z.area_land_meters,
+    z.county,
+    z.state_fips_code,
+    z.state_code,
+    z.state_name
+  FROM
+    `bigquery-public-data.geo_us_boundaries.zip_codes` AS z,
+    `bfro.bfro_reports_geocoded_csv` geo
+  WHERE
+    ST_WITHIN(ST_GEOGPOINT(geo.longitude,
+        geo.latitude),
+      z.zip_code_geom) ) AS zip
+ON
+  base.number=zip.number
+LEFT OUTER JOIN
+  `bigquery-public-data.census_bureau_usa.population_by_zip_2010` AS uscb
+ON
+  CAST(uscb.zipcode AS int64)=zip.zip_code_int64
+  AND uscb.gender IS NULL
+LEFT OUTER JOIN
+  `bfro.hardiness_zones` AS hz
+ON
+  hz.zip_code=zip.zip_code_int64
+LEFT OUTER JOIN
+  `bigquery-public-data.geo_us_boundaries.counties` counties
+ON
+  trim(lower(REPLACE(SPLIT(zip.county)[
+  OFFSET
+    (0)],'County','')))=trim(lower(REPLACE(counties.county_name,'County','')))
+  AND cast(zip.state_fips_code as int64)=cast(counties.state_fips_code as int64)
+LEFT OUTER JOIN (
+  SELECT
+    year,
+    county_fips,
+    ROUND(jan_value + feb_value + mar_value + apr_value + may_value + june_value 
+    + july_value + aug_value + sept_value + oct_value + nov_value + dec_value) AS yearly_precipitation
+  FROM
+    `bfro.precip_by_county_vv`) pcp
+ON
+  CAST(counties.county_fips_code AS int64)=CAST(pcp.county_fips AS int64)
+  AND EXTRACT(YEAR
+  FROM
+    base.date)=pcp.year
+LEFT OUTER JOIN
+  `bfro.elevations` elevations
+ON
+  elevations.number=base.number;
